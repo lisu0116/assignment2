0fcc3771d29374127f77d38cf7ee5072d0054728
Lisu Lee <intosuka83@gmail.com>
2024-11-08 17:16:46 -0800

    finished the code and added the reference url

--- project2/create-users.sh
+++ project2/create-users.sh
@@ -14,5 +14,68 @@ check_root() {
 # Function to display usage info
 usage() {
     echo "Usage: $0 -u USERNAME -s SHELL [-g GROUPS]"
-    echo 
+    echo "-u: specify username"
+    echo "-s: specify shell"
+    echo "-g: specify additional groups"
+    exit 1
 }
+# $0: represent the script's name
+
+# function to create a user
+create_user() {
+    local username=$1
+    local shell=$2
+    local groups=$3
+
+    useradd -m -s "$shell" "$username"
+    # create user with shell and home directory
+    # -m: create the user's home directory
+    # -s: define the shell that user will use
+    # https://man.archlinux.org/man/useradd.8
+
+    passwd "$username"
+    # create the password for the new user
+    # https://man.archlinux.org/man/passwd.1
+
+    if [ -n "$groups" ]; then
+        usermod-aG "$groups" "$username"
+    fi
+    # add user to additional groups if specified
+    # -a: append the user to the supplementary group(s), use only with -G option
+    # -G: specify a list of supplementary groups
+    # https://man.archlinux.org/man/usermod.8 and week 6 notes
+
+    cp -r /etc/skel/. "/home/$username/"
+    # cp: copy contents of /etc/skel/ to user's home directory
+    # -r: copy directories recursively
+    # https://man.archlinux.org/man/cp.1
+    
+    echo "user $username created"
+}
+
+check_root
+
+# parse command line option
+while getopts "u:s:g:" opt; 
+do
+    case $opt in
+    u) username=$OPTARG;;
+    s) shell=$OPTARG;;
+    g) groups=$OPTARG;;
+    *) usage;;
+    esac
+done
+# https://www.ibm.com/docs/sv/aix/7.1?topic=g-getopts-command
+# https://man.archlinux.org/man/getopts.1p
+# u:s:g - specify the script accepts options -u, -s, -g, each requiring argument (by colon)
+# opt: variable that stores each option character
+# u), s), g): store the provided argument
+# *): catch any unrecognized options and call the usage function
+# $OPTARG: variable set by getopts that contains the argument
+
+if [ -z "$username" ] || [ -z "$shell" ]; then
+    usage
+fi
+# check if required options are provided
+
+create_user "$username" "$shell" "$groups"
