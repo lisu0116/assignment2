0e80c963b22a33023f2a69b839e7c98ae5364633
Lisu Lee <intosuka83@gmail.com>
2024-11-08 17:38:10 -0800

    fixed the typo

--- project2/create-users.sh
+++ project2/create-users.sh
@@ -3,13 +3,17 @@
 # Function to check the root privileges
 check_root() {
     if [ "$EUID" -ne 0 ]; then
-        ehco "This must be run as root"
+        echo "This must be run as root"
         exit 1 # exit the script with the error status
+    fi
 }
 # $EUID is effective user ID of the current user
 # https://eitca.org/cybersecurity/eitc-is-lsa-linux-system-administration/linux-processes/processes-overview/examination-review-processes-overview/what-is-the-difference-between-user-id-uid-and-effective-user-id-euid-in-linux-processes/
 # Root user has an EUID 0
 # EUID allows for more precise control over process permissions and align's with Linux security model.
+# -ne: stands for not equal
+# if EUID doesn't have root privilege, it's not running the code.
+                                                                                                                                                                                                                        
 
 # Function to display usage info
 usage() {
@@ -38,7 +42,7 @@ create_user() {
     # https://man.archlinux.org/man/passwd.1
 
     if [ -n "$groups" ]; then
-        usermod-aG "$groups" "$username"
+        usermod -aG "$groups" "$username"
     fi
     # add user to additional groups if specified
     # -a: append the user to the supplementary group(s), use only with -G option
