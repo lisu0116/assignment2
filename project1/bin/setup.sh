d180962fa89f3578e7631483fcaa8a50a9044bba
Lisu Lee <intosuka83@gmail.com>
2024-11-10 20:05:51 -0800

    fixed the code and comments

--- project1/bin/setup.sh
+++ project1/bin/setup.sh
@@ -15,31 +15,32 @@ check_root() {
 # EUID allows for more precise control over process permissions and align's with Linux security model.
 # -ne: stands for not equal
 # if EUID doesn't have root privilege, it's not running the code.
-# since creating a new user, must have root privilege
+
 
 check_root # call the check_root function
 
 # parse command line option
-while getopts "u:s:g:" opt; 
+while getopts "is" opt; 
 do
     case $opt in
-    u) username=$OPTARG;;
-    s) shell=$OPTARG;;
-    g) groups=$OPTARG;;
+    i) ./assignment2/project1/bin/install-packages.sh;;
+    s) ./assignment2/project1/bin/create-symlinks.sh
     *) usage;;
     esac
 done
 # https://www.ibm.com/docs/sv/aix/7.1?topic=g-getopts-command
 # https://man.archlinux.org/man/getopts.1p
-# u:s:g - specify the script accepts options -u, -s, -g, each requiring argument (by colon)
+# is - specify that options -i, -s are valid
 # opt: variable that stores each option character
-# u), s), g): store the provided argument
+# -i: if it's provided, run the install-packages.sh
+# -s: if it's provided, run the create-symlinks.sh
 # *): catch any unrecognized options and call the usage function
 # $OPTARG: variable set by getopts that contains the argument
 
-if [ -z "$username" ] || [ -z "$shell" ]; then
+if [ $OPTIND -eq 1 ]; then
     usage
 fi
-# check if required options are provided
+# check if no options are provided, OPTIND is the index of the next argument to be processed
+# usage: showing the options if no oprions are given
 
 # https://tecadmin.net/execute-one-shell-script-from-another-shell-script/#google_vignette
